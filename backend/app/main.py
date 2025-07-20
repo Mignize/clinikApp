c3eb26a5d53c494d2dc488cc4a9436631a1dbe9c
Mignize <santiagosalazarxdd@gmail.com>
2025-07-20 16:09:08 -0500

    fix: update pre-commit version and initialized pre-commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
-from app.database import init_db, get_session
+
+from app.database import init_db
 
 app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup_event():
     init_db()
     print("Startup event: Database initialized.")
+
 
 @app.get("/")
 async def root():
