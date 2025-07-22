3dfe09d1242f9940bf287eb8d002fb06dd3bf311
Mignize <santiagosalazarxdd@gmail.com>
2025-07-22 09:51:20 -0500

    fix: delete logs and copy all on Docker

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,4 @@
-import os
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-print("DEBUG ENV ----")
-print("DATABASE_URL:", os.environ.get("DATABASE_URL"))
-print("SECRET_KEY:", os.environ.get("SECRET_KEY"))
-print("FRONTEND_URL:", os.environ.get("FRONTEND_URL"))
 
 
 class Settings(BaseSettings):
@@ -18,7 +11,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-print("From settings object:")
-print(settings.DATABASE_URL)
-print(settings.SECRET_KEY)
-print(settings.FRONTEND_URL)
